c3cbd5f73e518ec0f0d2a312a6d06cb66248cb0f
dakoyo <155788884+dakoyo@users.noreply.github.com>
2024-02-03 09:03:37 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,2 @@
-import express from "express";
-const app = express();
-const port = process.env.PORT || 3001;
-
-app.get("/", async (req, res) => {
-  try {
-    res.send("<h1>Hello, world<h1>");
-  } catch (e) {
-    console.warn(e)
-  }
-})
-
-// app.listen(port)
 
 import("./bot.js");
