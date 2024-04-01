7e36a90e1383aa43ec5cd3ffbf181decac4bc66d
dakoyo <155788884+dakoyo@users.noreply.github.com>
2024-04-01 12:24:49 +0000

    ping

--- bot/ping.js
+++ bot/ping.js
@@ -1,15 +1,22 @@
 import client from "../client.js";
+import Logger from "../util/logger.js";
 import config from "../config.js";
 
-client.on("messageCreate", async message => {
-    if (message.content == "!ping") {
-        try {
-            await message.channel.send("pong!");
-        } catch (err) {
+const logger = new Logger("ping");
 
+setTimeout(() => {
+
+    client.on("messageCreate", async message => {
+        if (message.content == "!ping") {
+            try {
+                await message.channel.send("pong!");
+            } catch (err) {
+    
+            }
         }
-    }
-    if ((message.channelId == config.dev.logChannelId) && (message.embeds[0]?.description == "```js\nlogin successful!```")) {
-        await client.destroy();
-    }
-})
+        if ((message.channelId == config.dev.logChannelId) && (message.embeds[0]?.description == "```js\nlogin successful!```")) {
+            await logger.log("client destroy")
+            await client.destroy();
+        }
+    })
+},2000)
