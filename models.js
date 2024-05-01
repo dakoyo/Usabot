52524b7b171315b2338b4cdb6d6730c4b8bce740
dakoyo <155788884+dakoyo@users.noreply.github.com>
2024-05-01 05:20:30 +0000

    Remove test

--- models.js
+++ models.js
@@ -137,24 +137,5 @@ new Model("にしぼっと2")
 })
 .register();
 
-new Model("うさぼっとFAST")
-.addPrompt(getPrompt("usabotFAST"))
-.setAvatarURL("https://ul.h3z.jp/d0wew77m.png")
-.setCommandOptionName("usabotfast")
-.setDescription("高速化したうさぼっとのモデル。ある程度の推論能力もある\nただし、続けて会話することはできない")
-.setMode("FAST")
-.onAsk(async ev => {
-    const { ids, gemini, imageBuffer, model, message} = ev;
-    const question = (message instanceof Message) ? message.content : message;
-    const res = await groq.chat.completions.create({
-        messages: [
-            {role: "system", content: getPrompt("usabotFAST")},
-            {role: "user", content: question}
-        ],
-        model: "Llama3-70b-8192"
-    })
-    return res;
-})
-.register();
 
 import("./client.js");
